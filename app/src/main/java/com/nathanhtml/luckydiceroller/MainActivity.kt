f689688e71848716995f67929a0cb92675214abd
Nathan Flacher <nathan.flacher.pro@gmail.com>
2022-09-08 19:20:50 +0200

    add dice images

--- app/src/main/java/com/nathanhtml/luckydiceroller/MainActivity.kt
+++ app/src/main/java/com/nathanhtml/luckydiceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nathanhtml.luckydiceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.nathanhtml.luckydiceroller.model.Dice
@@ -13,25 +14,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Lucky dice process
-        val dice : Dice = Dice(6);
-        val btn : Button = findViewById(R.id.roll_btn);
-        val luckyOrNoMsg : TextView = findViewById(R.id.lucky_or_not);
-        val luckyOrNoDetail : TextView = findViewById(R.id.lucky_or_not_detail);
-        val printDice : TextView = findViewById(R.id.print_dice);
+        val dice = Dice(6)
+        val btn : Button = findViewById(R.id.roll_btn)
+        val luckyOrNoMsg : TextView = findViewById(R.id.lucky_or_not)
+        val printBotDice : ImageView = findViewById(R.id.print_bot_dice)
+        val printYourDice : ImageView = findViewById(R.id.print_your_dice)
+        var IAResult : Int = dice.roll()
+        var UserResult : Int = dice.roll()
         val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-        toast.show();
+        toast.show()
         btn.setOnClickListener{
-            val IA = dice.roll();
-            val User = dice.roll();
-            printDice.text = User.toString();
-            luckyOrNoDetail.text = "I get " + IA.toString();
-            if(User > IA) {
+            // roll dices
+            IAResult = dice.roll()
+            UserResult = dice.roll()
+            // print IA result
+            if (IAResult == 1) printBotDice.setImageResource(R.drawable.dice_1)
+            else if (IAResult == 2) printBotDice.setImageResource(R.drawable.dice_2)
+            else if (IAResult == 3) printBotDice.setImageResource(R.drawable.dice_3)
+            else if (IAResult == 4) printBotDice.setImageResource(R.drawable.dice_4)
+            else if (IAResult == 5) printBotDice.setImageResource(R.drawable.dice_5)
+            else if (IAResult == 6) printBotDice.setImageResource(R.drawable.dice_6)
+            // print User result
+            if (UserResult == 1) printYourDice.setImageResource(R.drawable.dice_1)
+            else if (UserResult == 2) printYourDice.setImageResource(R.drawable.dice_2)
+            else if (UserResult == 3) printYourDice.setImageResource(R.drawable.dice_3)
+            else if (UserResult == 4) printYourDice.setImageResource(R.drawable.dice_4)
+            else if (UserResult == 5) printYourDice.setImageResource(R.drawable.dice_5)
+            else if (UserResult == 6) printYourDice.setImageResource(R.drawable.dice_6)
+            // print message
+            if(UserResult > IAResult) {
                 luckyOrNoMsg.text = "Lucky !"
             } else {
                 luckyOrNoMsg.text = "Unlucky !"
             }
-//            lucky_or_not
-//            rollText.text = dice.roll().toString();
         }
     }
 }
